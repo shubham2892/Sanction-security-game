a46f3c3572a8dee36652aa00893f94ddc6d64d41
Bennett Narron <bynarron@ncsu.edu>
2015-09-10 13:46:45 -0400

    Updated models and database schema

--- P1/Game/admin.py
+++ P1/Game/admin.py
@@ -3,11 +3,9 @@ from Game.models import *
 
 # Register your models here.
 admin.site.register(Player)
-admin.site.register(ResourceClassification)
 admin.site.register(ResearchResource)
-admin.site.register(ResearchTask)
-admin.site.register(AttackResource)
+admin.site.register(ResearchObjective)
 admin.site.register(SecurityResource)
-admin.site.register(AttackDeck)
+admin.site.register(AttackResource)
 admin.site.register(Game)
 
--- P1/Game/models.py
+++ P1/Game/models.py
@@ -1,90 +1,111 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import timedelta
+from random import choice
+from django.db import IntegrityError
+import random
 
 
 # A Player
 class Player(models.Model):
-    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=50, default=None)
+    last_name = models.CharField(max_length=50, default=None)
+    email = models.CharField(max_length=50, default=None, unique=True)
+
+    def __unicode__(self):
+        return u'%s %s (%s)' % (self.first_name, self.last_name, self.email)
 
     @property
     def name(self):
-        return self.user.get_username()
-
-    @property
-    def is_admin(self):
-        return self.is_superuser()
+        return "{} {}".format(self.first_name, self.last_name)
 
 
-# A Resource Classification used to match Security, Attack, and Research resources
-class ResourceClassification(models.Model):
-    classification = models.IntegerField()
-
-    @property
-    def name(self):
-        return self.classification()
-
+# Resource classifications by color
+RESOURCE_CLASSIFICATIONS = (
+    (1, "BLUE"),
+    (2, "RED"),
+    (3, "YELLOW"),
+)
 
 # A Research Resource for completing a Research Task
 class ResearchResource(models.Model):
-    name =  models.CharField(max_length = 25)
-    description = models.CharField(max_length = 150)
-    classification = models.ForeignKey(ResourceClassification)
+    classification = models.IntegerField(choices=RESOURCE_CLASSIFICATIONS)
 
-    @property
-    def name(self):
-        return self.name()
+    def __unicode__(self):
+        return u'%s Research Resource' % (self.classification)
 
 
 # A Security Resource for protecting against an Attack
 class SecurityResource(models.Model):
-    name =  models.CharField(max_length = 25)
-    description = models.CharField(max_length = 150)
-    classification = models.ForeignKey(ResourceClassification)
+    classification = models.IntegerField(choices=RESOURCE_CLASSIFICATIONS)
 
-    @property
-    def name(self):
-        return self.name()
+    def __unicode__(self):
+        return u'%s Security Resource' % (self.classification)
 
 
 # An Attack Resource for issuing an attack against a Research Resource
 class AttackResource(models.Model):
-    name =  models.CharField(max_length = 25)
-    description = models.CharField(max_length = 150)
-    classification = models.ForeignKey(ResourceClassification)
+    classification = models.IntegerField(choices=RESOURCE_CLASSIFICATIONS)
 
-    @property
-    def name(self):
-        return self.name()
+    def __unicode__(self):
+        return u'%s Attack Resource' % (self.classification)
 
 
 # A Research Task comprised of some number of Research Resources
-class ResearchTask(models.Model):
-    name = models.CharField(max_length=25)
+class ResearchObjective(models.Model):
+
+    RESEARCH_TASKS = (
+        (1, "Workshop"),
+        (2, "Conference"),
+        (3, "Journal"),
+    )
+
+    name = models.IntegerField(choices=RESEARCH_TASKS, default=None)
     required_resources = models.ManyToManyField(ResearchResource)
-    value = models.IntegerField()
-    deadline = models.IntegerField()
+    value = models.IntegerField(null=True, blank=True)
+    deadline = models.IntegerField(null=True, blank=True)
 
-    @property
-    def name(self):
-        return self.name()
+    def __unicode__(self):
+        return u'%s Objective' % (self.name)
 
-
-# A Deck of Attack Resources
-class AttackDeck(models.Model):
-    attack_resources = models.ManyToManyField(AttackResource)
+    def save(self, *args, **kwargs):
+        pass
 
 
 class Game(models.Model):
-    players = models.ManyToManyField(Player)
-    deck = models.ForeignKey(AttackDeck)
-    rounds = models.IntegerField()
-    duration = models.DurationField(default=timedelta(hours=1))
-    game_key = models.CharField(max_length=15)
+    game_key_length = 5
 
-    @property
-    def name(self):
-        return self.game_key()
+    players = models.ManyToManyField(Player)
+    ticks = models.IntegerField()
+    game_key = models.CharField(max_length=game_key_length, unique=True, null=True, blank=True, editable=False)
+
+    def __unicode__(self):
+        return u'%s' % (self.game_key)
+
+    def generate_random_alphanumeric(self, length=game_key_length):
+        return ''.join(random.choice('0123456789ABCDEF') for i in range(length))
+
+    def save(self, *args, **kwargs):
+        if not self.game_key:
+            self.game_key = self.generate_random_alphanumeric()
+            # using your function as above or anything else
+        success = False
+        failures = 0
+        while not success:
+            try:
+                super(Game, self).save(*args, **kwargs)
+            except IntegrityError:
+                 failures += 1
+                 if failures > 5: # or some other arbitrary cutoff point at which things are clearly wrong
+                     raise
+                 else:
+                     # looks like a collision, try another random value
+                     self.game_key = self.generate_random_alphanumeric()
+            else:
+                 success = True
+
+
+
 
 
 
