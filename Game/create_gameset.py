7b919024cacd8201143b5a155f308387dde59567
Shubham <shubham2892@gmail.com>
2017-12-20 08:00:15 +0530

    attack frequency changed

--- Game/create_gameset.py
+++ Game/create_gameset.py
@@ -4,12 +4,16 @@ from Game.models import Game, INDIVIDUAL_SANC, GROUP_SANC, Player, GameSet
 
 
 def create_game_set(player_ids):
+    TOTAL_TICKS = 35
+    ATTACK_FREQUENCY = 30
     game_1_demo = Game.objects.create(total_ticks=25, manager_sanc=INDIVIDUAL_SANC, attack_frequency=40)
     game_2_demo = Game.objects.create(total_ticks=25, manager_sanc=GROUP_SANC, attack_frequency=40)
-    game_1 = Game.objects.create(total_ticks=40, manager_sanc=INDIVIDUAL_SANC, attack_frequency=30)
-    game_2 = Game.objects.create(total_ticks=40, manager_sanc=INDIVIDUAL_SANC, attack_frequency=30)
-    game_3 = Game.objects.create(total_ticks=40, manager_sanc=GROUP_SANC, attack_frequency=30)
-    game_4 = Game.objects.create(total_ticks=40, manager_sanc=GROUP_SANC, attack_frequency=30)
+    game_1 = Game.objects.create(total_ticks=TOTAL_TICKS, manager_sanc=INDIVIDUAL_SANC,
+                                 attack_frequency=ATTACK_FREQUENCY)
+    game_2 = Game.objects.create(total_ticks=TOTAL_TICKS, manager_sanc=INDIVIDUAL_SANC,
+                                 attack_frequency=ATTACK_FREQUENCY)
+    game_3 = Game.objects.create(total_ticks=TOTAL_TICKS, manager_sanc=GROUP_SANC, attack_frequency=ATTACK_FREQUENCY)
+    game_4 = Game.objects.create(total_ticks=TOTAL_TICKS, manager_sanc=GROUP_SANC, attack_frequency=ATTACK_FREQUENCY)
 
     for player_id in player_ids:
         user = User.objects.create(username=player_id, password=player_id)
@@ -21,5 +25,4 @@ def create_game_set(player_ids):
         player_4 = Player.objects.create(user=user, game=game_4)
 
         GameSet.objects.create(user=user, demo_game_1=game_1_demo, demo_game_2=game_2_demo,
-                                          game_1=game_1, game_2=game_2, game_3=game_3, game_4=game_4)
-
+                               game_1=game_1, game_2=game_2, game_3=game_3, game_4=game_4)
